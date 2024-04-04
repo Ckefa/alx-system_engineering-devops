b4820dc53a7685f456525c75b9453a341bb67da6
ckefa <ckea65@gmail.com>
2024-04-04 03:00:59 -0700

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,19 @@
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
+#puppet advance
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
 }
-
-# Replace Nginx default configuration
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  source  => 'puppet:///modules/nginx/nginx.conf',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+-> package {'nginx':
+  ensure => present,
 }
-
-# Create a custom Nginx configuration file
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => file,
-  content => 'server {
-                listen 80 default_server;
-                listen [::]:80 default_server;
-                server_name _;
-                location / {
-                  add_header X-Served-By $hostname;
-                  root /app;
-                  index index.html;
-                }
-              }',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+-> file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
 }
-
-# Restart Nginx service when configuration changes
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  require   => Package['nginx'],
-  subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/sites-enabled/default'],
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
