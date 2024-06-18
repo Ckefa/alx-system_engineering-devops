5479b6d2df3ec84920d50d814404ed430b3f303b
ckefa <ckefa65@gmail.com>
2024-06-18 16:41:59 +0300

    submit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+
 """
 prints the titles of the first 10 hot posts listed for a given subreddit
 """
+
 
 from requests import get
 
@@ -31,3 +33,4 @@ def top_ten(subreddit):
 
     except Exception:
         print("None")
+
